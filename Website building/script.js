389eafd90d41da5b55e1a09c69a255f93aeb8ab8
astridschmerler-star <astridschmerler@gmail.com>
2026-04-13 16:18:35 +0000

    i

---
+++ Website building/script.js
@@ -0,0 +1,10 @@
+var i = 0;
+var txt = 'Sonic Echo is an interactive website that allows you, the user, to find music that suits your tastes. There is something here for all!';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        
+        document.getElementById("")
+    }
+}
